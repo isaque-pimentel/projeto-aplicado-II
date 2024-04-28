58788db44f43fd7e69e6b2436fec6b41f46c9607
ipimx <pimentel.isaque@gmail.com>
2024-04-28 15:06:10 -0300

    Load test image, make prediction and show image with bounding box

--- scripts/detect_object.py
+++ scripts/detect_object.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 import os
 import tensorflow as tf
+import plotly.express as px
+import cv2
 
 from shutil import copy
 from tensorflow.keras.models import Model
@@ -11,6 +13,7 @@ from tensorflow.keras.callbacks import TensorBoard
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.applications import InceptionResNetV2
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 # Set directory constants
 file_dir = os.path.dirname(os.path.abspath(__file__))
@@ -24,5 +27,44 @@ print(f"Load model from {model_filename}")
 model2 = tf.keras.models.load_model(model_filename)
 print("Model loaded successfully")
 
+# Load test image
 test_path = os.path.join(input_dir, "TEST", "TEST.jpeg")
 image = load_img(test_path)
+image = np.array(image, dtype=np.uint8)
+h, w, d = image.shape
+
+print(f"Altura da imagem TEST = {h}")
+print(f"Largura da imagem TEST = {w}")
+
+# Show test image
+fig = px.imshow(image)
+fig.update_layout(width=700, height=500,  margin=dict(l=10, r=10, b=10, t=10))
+fig.show()
+
+# Load and normalize
+load_image = load_img(test_path, target_size=(224, 224))
+load_image_arr = img_to_array(load_image) / 255.0
+print(f"Tamanho da imagem TEST = {load_image_arr.shape}")
+
+test_arr = load_image_arr.reshape(1,224,224,3)
+print(f"Tamanho da imagem TEST (Input) = {test_arr.shape}")
+
+# Make prediction
+coords = model.predict(test_arr)
+denorm = np.array([w,w,h,h])
+coords = coords * denorm
+
+# Creating box
+coords = coords.astype(np.int32)
+
+# Draw bounding on top the image
+xmin, xmax,ymin,ymax = coords[0]
+pt1 =(xmin,ymin)
+pt2 =(xmax,ymax)
+print(pt1, pt2)
+
+# Show test image with box
+cv2.rectangle(image,pt1,pt2,(0,255,0),3)
+fig = px.imshow(image)
+fig.update_layout(width=700, height=500, margin=dict(l=10, r=10, b=10, t=10))
+fig.show()
