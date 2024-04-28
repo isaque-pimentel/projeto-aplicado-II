8c13fcd77c82b79b23a5c813d483d27765e4e418
ipimx <pimentel.isaque@gmail.com>
2024-04-28 20:00:28 -0300

    Apply recognize plate for a list of images

--- scripts/recognize_plate.py
+++ scripts/recognize_plate.py
@@ -26,12 +26,17 @@ def recognize_plate(path, show=False):
         fig.update_layout(width=700, height=500, margin=dict(l=10, r=10, b=10, t=10))
         fig.show()
     # OCR using Tesseract
-    plate_text = pt.image_to_string(plate_arr, lang='eng')
+    plate_text = pt.image_to_string(plate_arr) # , lang='eng')
     return plate_text
-
 
 # Load test plage image
 test_dir = os.path.join(input_dir, "TEST")
-test_plate_path = os.path.join(test_dir, "TEST_PLATE_3.jpeg")
-plate_text = recognize_plate(test_plate_path)
-print(plate_text)
+
+filenames = ["TEST_1", "TEST_2", "TEST_3", "TEST_4"]
+
+for filename in filenames:
+    # Save test plate
+    filename_plate = f"{filename}_PLATE"
+    test_plate_path = os.path.join(test_dir, ".".join((filename_plate, "jpeg")))
+    plate_text = recognize_plate(test_plate_path)
+    print("Plate:"+fr"{plate_text}")
