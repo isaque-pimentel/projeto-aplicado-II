27e2cd5d0a075ef6df7db907556563a98c801fb0
ipimx <pimentel.isaque@gmail.com>
2024-04-28 15:57:05 -0300

    create plate recognition pipeline

--- scripts/recognize_plate.py
+++ scripts/recognize_plate.py
@@ -17,15 +17,21 @@ file_dir = os.path.dirname(os.path.abspath(__file__))
 input_dir = "dataset"
 output_dir = "output"
 
+# Create Plate Recognition Pipeline
+def recognize_plate(path, show=False):
+    # Load plate image
+    plate_arr = np.array(load_img(test_plate_path))
+    if show:
+        fig = px.imshow(plate_arr)
+        fig.update_layout(width=700, height=500, margin=dict(l=10, r=10, b=10, t=10))
+        fig.show()
+    # OCR using Tesseract
+    plate_text = pt.image_to_string(plate_arr, lang='eng')
+    return plate_text
+
+
 # Load test plage image
 test_dir = os.path.join(input_dir, "TEST")
 test_plate_path = os.path.join(test_dir, "TEST_PLATE.jpeg")
-plate_arr = np.array(load_img(test_plate_path))
-
-fig = px.imshow(plate_arr)
-fig.update_layout(width=700, height=500, margin=dict(l=10, r=10, b=10, t=10))
-fig.show()
-
-# OCR using Tesseract
-plate_text = pt.image_to_string(plate_arr, lang='eng')
+plate_text = recognize_plate(test_plate_path)
 print(plate_text)
